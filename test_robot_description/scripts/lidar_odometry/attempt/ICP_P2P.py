b5cc961bb194ae42c003387ae7b4be58a104928c
SIDDHARTH-S-001 <73553742+SIDDHARTH-S-001@users.noreply.github.com>
2024-04-09 17:09:18 +0530

    created scripts to make different attempts for point to point ICP

--- test_robot_description/scripts/lidar_odometry/attempt/ICP_P2P.py
+++ test_robot_description/scripts/lidar_odometry/attempt/ICP_P2P.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
 import rospy
 import numpy as np
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.point_cloud2 import read_points
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf.transformations import quaternion_from_euler, quaternion_from_matrix
 from sklearn.neighbors import NearestNeighbors
 from geometry_msgs.msg import PoseStamped
 import tf2_ros
@@ -20,16 +19,15 @@ class LidarICP:
         self.prev_scan = None
         self.odom_icp_pose = PoseStamped()  # Initialize odom_icp pose to zeros
         self.odom_icp_pose.header.frame_id = "odom_icp"
+        self.H_mat = np.identity(4)
 
     def scan_callback(self, scan_msg):
-        current_scan = self.laser_scan_to_point_cloud(scan_msg) # current_scan is an array of points
-        
+        current_scan = self.laser_scan_to_point_cloud(scan_msg) # current_scan is an array of points        
         if self.prev_scan is not None:
             # Apply ICP
-            R, T = self.point_to_point_icp(self.prev_scan, current_scan)
-            
+            H, R, t = self.point_to_point_icp(self.prev_scan, current_scan)            
             # Update pose
-            self.update_pose(R, T)
+            self.update_pose(H, R, t)
         
         # Save the current scan for the next iteration
         self.prev_scan = current_scan
@@ -38,7 +36,6 @@ class LidarICP:
         # Extract points from LaserScan message
         ranges = np.array(scan_msg.ranges)
         angles = np.arange(scan_msg.angle_min, scan_msg.angle_max, scan_msg.angle_increment)
-
         # Convert polar coordinates to Cartesian coordinates
         points = []
         for i in range(len(ranges)):
@@ -48,52 +45,56 @@ class LidarICP:
                 points.append([x, y])
 
         return np.array(points)
-
-
+    
     def point_to_point_icp(self, source, target):
         # Use Nearest Neighbors to find correspondences
         nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(target)
         distances, indices = nbrs.kneighbors(source)
-
         # Extract matched points
-        matched_target = target[indices.flatten()]
-        
+        matched_target = target[indices.flatten()]        
         # Compute the transformation
-        R, T = self.calculate_icp_transformation(source, matched_target) # prev_scan is the source and current_scan is target
+        H, R, t = self.calculate_icp_transformation(source, matched_target) # prev_scan is the source and current_scan is target
 
-        return R, T
-
+        return H, R, t
+    
     def calculate_icp_transformation(self, source, target):
         # Calculate the transformation between source and target using SVD
         # prev_scan is the source and current_scan is target
         mean_source = np.mean(source, axis=0)
         mean_target = np.mean(target, axis=0)
-
         centered_source = source - mean_source
         centered_target = target - mean_target
-
         shape_val = source.shape[1] # get number of dimensions
-
         W = np.dot(centered_source.T, centered_target)
-
         U, _, Vt = np.linalg.svd(W)
-
-        R = np.dot(Vt.T, U.T)
-
+        R_val = np.dot(Vt.T, U.T)
         # special reflection case
-        if np.linalg.det(R) < 0:
+        if np.linalg.det(R_val) < 0:
             Vt[shape_val-1,:] *= -1
-            R = np.dot(Vt.T, U.T)
+            R_val = np.dot(Vt.T, U.T)
+        t = mean_target - np.dot(R_val, mean_source)
 
-        T = mean_target.T - np.dot(R, mean_source.T)
+        R = np.zeros((3, 3))
+        R[:2, :2] = R_val
+        R[2, 2] = 1
 
-        return np.round(R, 3), np.round(T, 3)
+        H = np.identity(4) # homogeneous transformation matrix
+        H[:3, :3] = R
+        H[:2, 3] = t
+        H[3, 3] = 1
 
-    def update_pose(self, R, T):
+        print('H: ', np.round(H, 3))
+
+        return np.round(H, 3),np.round(R, 3), np.round(t, 3)
+    
+    def update_pose(self,H, R, t):
         # Update the pose with the ICP transformation
-        self.odom_icp_pose.pose.position.x += T[0]
-        self.odom_icp_pose.pose.position.y += T[1]
-        updated_yaw = np.arctan2(R[1, 0], R[0, 0])  # Extracting rotation from the transformation matrix
+        self.H_mat *= H
+        trans = H[:3, 3]
+        Rot = H[:3, :3]
+        self.odom_icp_pose.pose.position.x = trans[0][0]
+        self.odom_icp_pose.pose.position.y = trans[1][0]
+        updated_yaw = np.arctan2(Rot[1, 0], Rot[0, 0])  # Extracting rotation from the transformation matrix
         q = quaternion_from_euler(0, 0, updated_yaw)
         self.odom_icp_pose.pose.orientation.x = q[0]
         self.odom_icp_pose.pose.orientation.y = q[1]
